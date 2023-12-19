bd06a2373f2fa0828d423c5569012d91fa4abeb2
saulsneider7 <saulsneider@hotmail.com>
2023-12-18 19:26:54 -0500

    first commit

---
+++ mi_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('ingresar_persona/',views.ingresar_persona, name='ingresar_persona'),
+    path('ver_personas/',views.ver_personas, name='ver_personas'),
+]
